772c110c131ab87345d1b584f9dc86b3af801696
samuel <damosisamuel@gmail.com>
2020-12-11 15:35:08 +0100

    done

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@
 var nome = prompt("Inserisci il tuo nome");
 var cognome = prompt("Inserisci il tuo cognome");
 var colorPreferito =  prompt('Inserisci il tuo colore Preferito ?');
-var risulatati = nome +" "+ cognome + " " + colorPreferito;
+var risulatati = nome + cognome + colorPreferito;
 // nome,cognome,colore
 
 document.getElementById("nome").innerHTML = "ciao" + " " + nome + " " + cognome;
-document.getElementById("cognome").innerHTML ="i tuoi dettagli sono  " + risulatati + "19";
+document.getElementById("cognome").innerHTML ="i tuoi dettagli sono" + risulatati + "19";
+
+
 
 alert("thank you and goodbye"+ " " + nome + " " + " " + cognome);
 
